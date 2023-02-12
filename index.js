ff41fab83f402d4638657fc4f04ed80cefedddf1
vhm205 <minhhuynhvu9221@gmail.com>
2023-02-12 15:02:07 +0700

    chore: convert node-telegram-api to telegraf

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
 import "dotenv/config.js";
-import TelegramBotApi from 'node-telegram-bot-api';
+import { Telegraf } from 'telegraf';
 import { getPhotosByTopic, askOpenAI, sleepCalculator } from './commands/index.js';
 
-const bot = new TelegramBotApi(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 const listenEvent = () => {
+  // bot.command('start', ctx => {
+  //   console.log({ ctx });
+  //   console.log({ message: ctx.update.message })
+  //   console.log({ options: ctx.telegram.options })
+  //   bot.telegram.sendMessage(ctx.chat.id, 'hello there! Welcome to my new telegram bot.', {
+  //   })
+  // })
+
+  // bot.hears(/hi/gi, ctx => {
+		// console.log({ ctx });
+		// console.log({ message: ctx.update.message });
+		// console.log({ options: ctx.telegram.options });
+  // });
+
+  // bot.on(/\/bed (.+)/, (ctx) => {
+  //   console.log({ ctx });
+		// console.log({ message: ctx.update.message });
+		// console.log({ options: ctx.telegram.options });
+  // })
+
 	sleepCalculator(bot);
 	askOpenAI(bot);
 	getPhotosByTopic(bot);
 
-	// bot.on('message', async msg => {
-	// 	const chatId = msg.chat.id;
-	// const text = msg.text;
-	// 	bot.sendMessage(chatId, text);
-	// });
+  bot.start((ctx) => ctx.reply("Welcome to VHM's bot"));
+	bot.help((ctx) => ctx.reply('Send me a sticker'));
 
-	bot.on('polling_error', (error) => {
-		console.log(error.code); // => 'EFATAL'
-	});
-
-	bot.on('webhook_error', (error) => {
-		console.log(error.code); // => 'EPARSE'
+	bot.on('message', async (ctx) => {
+		const message = ctx.update.message.text;
+		if (message.match(/hello/)) {
+			ctx.reply('Xin chào');
+		} else {
+			ctx.reply('Hong hiểu...');
+		}
 	});
 
 	console.log('Bot listening...');
+	bot.launch();
 };
 
 listenEvent();
